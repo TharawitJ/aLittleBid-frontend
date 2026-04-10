901eb042c99ce488bd15353993ef57fee81c6cb0
Tassanan-K <tussanan.k@ku.th>
2026-04-10 15:45:23 +0700

    style: mockup ui done2

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import MainLayout from "../layouts/mainLayout.jsx";
 import  {createBrowserRouter} from "react-router";
-import UserPage from "../pages/user_page"
+import UserPage from "../pages/user_page";
 import HomePage from "../pages/HomePage.jsx";
 import AuctionPage from "../pages/AuctionPage.jsx";
 
