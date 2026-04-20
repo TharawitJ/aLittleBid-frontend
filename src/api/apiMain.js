9c18226b82c509ee3b5f3a1626049bb51e5a73bd
TharawitJ <thara10.tharawit@gmail.com>
2026-04-20 18:10:24 +0700

    feat: implement auction management features and integrate with product display

--- src/api/apiMain.js
+++ src/api/apiMain.js
@@ -28,10 +28,16 @@ export const apiEditUserAddressById = (userId, addressId, data) =>
 export const apiEditUserProfileById = (userId, authId, data) => mainApi.patch(`/users/${userId}`,data);
 
 // PRODUCTS
-// export const app.use('/api/products', productRoutes);
 export const apiGetCategories = () => mainApi.get(`/products/categories`);
 export const apiGetAllProducts = () => mainApi.get(`/products`);
 export const apiGetProductsById = (productId) => mainApi.get(`/products/${productId}`);
 export const apiCreateProduct = () => mainApi.post(`/products`);
 export const apiUpdateProduct = (productId) => mainApi.patch(`/products/${productId}`);
 export const apiDeleteProduct = (productId) => mainApi.delete(`/products/${productId}`);
+
+// AUCTION
+export const apiGetAllAuction = () => mainApi.get(`/auctions`);
+export const apiCreateAuction = () => mainApi.post(`/auctions`);
+export const apiGetAuctionById = (auctionId) => mainApi.get(`/auctions/${auctionId}`);
+export const apiUpdateAuction = (auctionId) => mainApi.patch(`/auctions/${auctionId}`);
+export const apiDeleteAuction = (auctionId) => mainApi.delete(`/auctions/${auctionId}`);
