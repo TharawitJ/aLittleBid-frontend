b98566525e378dfcd0b3fb108acba10707d3c3a7
Tassanan-K <tussanan.k@ku.th>
2026-04-22 12:16:45 +0700

    resolved conflig

--- src/pages/ProductDetailBid.jsx
+++ src/pages/ProductDetailBid.jsx
@@ -155,7 +155,6 @@ const ProductDetailBid = () => {
                 </div>
 
                 <form onSubmit={handleSubmit(hdlOnSubmit)}>
-                <form>
                   <div className="space-y-6">
                     <div className="space-y-2">
                       <label className="font-['Manrope'] uppercase tracking-widest text-[10px] text-stone-500">Your Bid</label>
