fad4f816935f8507a9da7cb4ab2ffa5c500c92a3
TharawitJ <thara10.tharawit@gmail.com>
2026-04-27 19:18:17 +0700

    feat: Update auction data handling and improve logging in AuctionBid and DisplayProducts components

--- src/pages/AuctionBid.jsx
+++ src/pages/AuctionBid.jsx
@@ -63,8 +63,12 @@ const AuctionBid = () => {
     connectSocket();
     joinAuctionRoom(auctionId);
     getAuctionById(auctionId);
-    getProductsById(auctionById?.productId);
+    // getProductsById(auctionById?.productId);
 
+    setIsLoading(false);
+    
+    console.log('auctionById', auctionById)
+    console.log('auctionId', auctionId)
     if (!auctionById || String(auctionById?.id) !== String(auctionId)) {
       console.warn(
         `[AuctionBid] auctionId mismatch: expected ${auctionId}, got ${auctionById?.id}. Join cancelled.`,
@@ -72,7 +76,6 @@ const AuctionBid = () => {
       return;
     }
     getAllUser();
-    setIsLoading(false);
     return () => {
       leaveAuctionRoom();
     };
@@ -96,7 +99,7 @@ const AuctionBid = () => {
               <div className="relative group">
                 <div className="aspect-[4/5] md:aspect-[3/2] overflow-hidden rounded-lg bg-[#f6f3f2]">
                   <img
-                    src={images?.[0]?.imageUrl}
+                    src={auctionById.product.images}
                     alt="Artwork"
                     className="w-full h-full object-cover"
                   />
