6a6e949e95951294087df5c83deb8f40dad0b070
tingchannavong <tingchannavong.gmail.com>
2026-04-27 19:21:22 +0700

    added ? to product

--- src/pages/AuctionBid.jsx
+++ src/pages/AuctionBid.jsx
@@ -117,10 +117,10 @@ const AuctionBid = () => {
                   {filterCategoryName[0]?.name}
                 </span>
                 <h1 className="text-5xl md:text-6xl font-['Noto_Serif'] text-[#1c1b1b] leading-tight">
-                  {auctionById?.product.name}
+                  {auctionById?.product?.name}
                 </h1>
                 <p className="text-xl font-['Noto_Serif'] italic text-[#5e5e5e]">
-                  {auctionById?.product.description}
+                  {auctionById?.product?.description}
                 </p>
               </div>
 
