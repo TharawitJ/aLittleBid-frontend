ab30df9735cd1874f1c2f57649fc7e851994ce5a
Tassanan-K <tussanan.k@ku.th>
2026-04-28 21:01:55 +0700

    feat: add debug logging for auction data and refine badge display in product listings

--- src/components/productPage/DisplayProducts.jsx
+++ src/components/productPage/DisplayProducts.jsx
@@ -43,11 +43,13 @@ function DisplayProducts({ displayProducts, allCategories }) {
   return (
     <>
       {displayProducts.map((i, idx) => {
+        console.log('allAuction-inreturn', allAuction)
+        console.log('i.id', i.id)
         const category = allCategories.find((cat) => cat.id === i.categoryId);
         const auction = allAuction?.find((a) => a.productId === i.id);
-        console.log("auction",auction);
+        console.log("auction", auction);
         const lastBid = bidData?.findLast((b) => b.auctionId === auction?.id);
-        console.log("lastBid", i.name, lastBid);
+        // console.log("lastBid", i.name, lastBid);
         const auctionId = auction?.id;
         return (
           <AuctionCard
@@ -57,7 +59,7 @@ function DisplayProducts({ displayProducts, allCategories }) {
             title={i.name}
             cat={category?.name}
             description={i.description}
-            // badge={auction?.status}
+            badge={auction?.status}
             price={hldPriceLoading(lastBid, auction)}
             timeLeft={<TimeCountdown product={i} />}
             auctionDetail={auction}
