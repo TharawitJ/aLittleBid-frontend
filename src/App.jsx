f4140d96429de9c0a7d15154c8a65f8e3a2ec8b5
tingchannavong <tingchannavong.gmail.com>
2026-05-12 18:51:28 +0700

    added useeffect import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { RouterProvider } from "react-router";
 import AppRouter from "./routes/AppRouter.jsx";
 import useUserStore from "./stores/user.store.js";  
